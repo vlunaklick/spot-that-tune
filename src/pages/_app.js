dcc08ae1f4adb1399716260985309f61837a09fd
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-02-06 09:01:05 -0300

    initial setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        font-family: ${inter.style.fontFamily};
+      `}</style>
+    </>
+  )
 }
